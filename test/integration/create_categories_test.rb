b735bac95ab827d199136bc9412f0794ddb56901
Juan Padilla <juan@llt-group.com>
2018-09-04 14:50:41 -0500

    add admin requirement to create

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,12 +1,16 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = User.create(username: 'john', email: 'john@example.com', password: 'password', admin: true)
+  end
 
-  test 'get new category form and create category' do 
+  test 'get new category form and create category' do
+    sign_in_as(@user, 'password')
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
-      post categories_path, params: { category: { name: 'sports' }}
+      post categories_path, params: { category: { name: 'sports' } }
       follow_redirect!
     end
     assert_template 'categories/index'
@@ -14,6 +18,7 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
   end
 
   test 'invalid category submission results in failure' do
+    sign_in_as(@user, 'password')
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
